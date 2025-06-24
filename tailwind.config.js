ad9040cba9b874aabdf43e6ed3bcaf12fae90870
Dev Ruhela <144375524+DevRuhela028@users.noreply.github.com>
2025-06-24 17:16:40 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx,html}"], 
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
